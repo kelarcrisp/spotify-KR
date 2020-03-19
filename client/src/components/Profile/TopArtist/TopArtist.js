bd3218b61491e0d93df25b631107aa676a465172
rijaz <redaij@bu.edu>
2020-03-19 13:27:12 -0700

    UI changes

--- client/src/components/Profile/TopArtist/TopArtist.js
+++ client/src/components/Profile/TopArtist/TopArtist.js
@@ -7,8 +7,10 @@ const TopArtist = props => {
                 <li style={{ listStyleType: 'none' }}>
                     <div className={classes.TopArtistInfo}>
                         <img src={props.imageSrc} />
-                        <div>{props.artistName}</div>
-                        <div>{props.artistGenre}</div>
+                        <div className={classes.ArtistNameGenre}>
+                            <div>{props.artistName}</div>
+                            <div>{props.artistGenre}</div>
+                        </div>
                     </div>
                 </li>
             </ul>
